2b6e5fded6c3052fc888423194da9a35d7a3775c
trigork <trigork@gmail.com>
2019-10-18 16:17:05 +0200

    FInished validation script

--- aspcluster_validate.py
+++ aspcluster_validate.py
@@ -73,13 +73,48 @@ def solve_asprin(asp_program, asp_facts, clingo_args):
                 break
         return rules
 
+def predict_class(rules, value):
+    # Remember to use factor
+    pred_classes = []
+    for cl,rls in rules.items():
+        for rl in rls:
+            if all(value[attr]*FACTOR >= val[0] and 
+                value[attr]*FACTOR <= val[1] for attr,val in rl.items()):
+                pred_classes += [cl]          
+    return tuple(pred_classes)
+
 def test_rules(rules, train_df):
-    # TODO: Use created rules to test the train_df
-    print(rules)
-    print(train_df)
-    print("ALL GREEN")
-            
-            
+    confusion = { c:{ c:0 for c in list(rules.keys()) + ['unclassified'] } for c in rules.keys() }
+    for i, row in train_df.iterrows():
+        pred = predict_class(rules, row)
+        real = row["species"].replace('-','_').lower()
+        if len(pred) > 0:
+            for p in pred:
+                confusion[real][p] += 1
+        else:
+            confusion[real]['unclassified'] += 1
+    return confusion
+
+def class_stats(confusion_dict):
+    stats = { c: { 'tp': 0, 'fp': 0, 'fn': 0 } for c in confusion_dict.keys() }
+    for cl, pred in confusion_dict.items():
+        stats[cl]['tp'] = pred[cl]
+        for pcl,val in pred.items():
+            if cl != pcl:
+                stats[cl]['fn'] += val
+                if pcl != 'unclassified':
+                    stats[pcl]['fp'] += val
+    for cl, st in stats.items():
+        if (st['tp']+st['fp']) > 0:
+            stats[cl]['precision'] = st['tp']/(st['tp']+st['fp'])
+        else:
+            stats[cl]['precision'] = 0
+        if (st['tp']+st['fn']) > 0:
+            stats[cl]['recall'] = st['tp']/(st['tp']+st['fn'])
+        else:
+            stats[cl]['recall'] = 0
+    return stats
+
 def main():
     os.environ["PYTHONUNBUFFERED"] = "TRUE"
 
@@ -107,6 +142,7 @@ def main():
     df = pd.read_csv(args.file)
     kf = KFold(n_splits = args.kfolds, shuffle = True, random_state = 2)
 
+    class_avg_stats = { }
     for train_idx,test_idx in kf.split(df):
         train = df.iloc[train_idx]
         test =  df.iloc[test_idx]
@@ -141,8 +177,29 @@ def main():
             options += ['--approximation='+ str(args.mode) ]
 
         optimum_rules = solve_asprin('rectangles_asprin', [asp_facts, asp_selected_parameters], options)
+        conf = test_rules(optimum_rules, test)
+        stats = class_stats(conf)
+        for cl, st in stats.items():
+            if cl not in class_avg_stats:
+                class_avg_stats.update({ cl : {'precision' : st['precision'], 'recall' : st['recall']} })
+            else:
+                class_avg_stats[cl]['precision'] += st['precision']
+                class_avg_stats[cl]['recall'] += st['recall']
+    avg_precision = 0
+    avg_recall = 0
+    numclasses = len(class_avg_stats.keys())
+    for cl, st in class_avg_stats.items():
+        class_avg_stats[cl]['precision'] = st['precision']/args.kfolds
+        class_avg_stats[cl]['recall'] = st['recall']/args.kfolds
+        avg_precision += class_avg_stats[cl]['precision']
+        avg_recall += class_avg_stats[cl]['recall']
+    avg_precision = avg_precision/numclasses
+    avg_recall = avg_recall/numclasses
 
-        test_rules(optimum_rules, train)
+    print(class_avg_stats, avg_precision, avg_recall)
+
+        
+        
     
     os.environ["PYTHONUNBUFFERED"] = "FALSE"
 
